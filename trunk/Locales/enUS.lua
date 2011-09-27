677c11af9468ce3a7698751afa526cfdd65f0daa
nagboy87 <nagboy87@c5eac2c9-6d14-461b-96f5-a79bdf918f32>
2011-09-27 18:03:40 +0000

    Forot to include updated enUS.lua

--- trunk/Locales/enUS.lua
+++ trunk/Locales/enUS.lua
@@ -1,22 +1,43 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("FriendXP", "enUS", true)
 
 L["Enabled"] = true
-L["Disabled"] = "Disabled"
-L["Debug"] = "Debug"
-L["Check Online Status"] = "Check Online Status"
-L["Send to all friends"] = "Send to all friends"
-L["Send to guild"] = "Send to guild"
+L["Toggle Lock"] = true
+L["UnlockDesc"] = "Toggle the frame lock for positioning."
+L["Disabled"] = true
+L["Debug"] = true
+L["Check Online Status"] = true
+L["Send to all friends"] = true
+L["Send to guild"] = true
 L["Send to entire guild, must be enabled to receive guild experience"] = true
-L["Only allow friends"] = "Only allow friends"
+L["Only allow friends"] = true
 L["AddFriend"] = "Add Friend"
-L["AddFriend_Desc"] = "Enter name and hit enter, must be on same realm as they are"
+L["AddFriend_Desc"] = "Enter name and hit enter, must be on same realm as they are (only works if send to all friends is disabled)"
+L["Ignore Whispers"] = true
+L["IgnoreWhisper_Desc"] = "Ignore all whisper messages.  Enabling this will only allow party and guild messages to be received."
+
+L["Frame Strata"] = true
+L["Frame Level"] = true
 
 L["FriendTTL"] = "Friend TTL"
 L["FriendTTL_Desc"] = "How long until an inactive friend is purged from the list in seconds"
 
+L["Width"] = true
+L["Height"] = true
 
+L["Miniframe Style"] = true
+L["Size Limits"] = true
+L["Position"] = true
+L["Miscellaneous"] = true
+L["Size and Position"] = true
+L["Bar Style"] = true
+L["Header Font Style"] = true
+L["Normal Font Style"] = true
+L["Font"] = true
 L["FontFace"] = "Font Face"
 L["FontStyle"] = "Font Style"
+L["FontSize"] = "Font Size"
+L["FontColor"] = "Font Color"
+L["LDB Tooltip"] = true
 L["Tooltip"] = "Tooltip"
 
 L["XPDisabled"] = "XP Disabled"
